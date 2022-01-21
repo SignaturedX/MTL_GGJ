145b8b3492c23b7eca2a34b1b53c8bdf37af8f6a
yungkiwi <kafountain5@gmail.com>
2022-01-20 20:36:28 -0500

    Code import
    hehe <3

---
+++ globalgamejam/Main.gd
@@ -0,0 +1,28 @@
+extends Node
+
+onready var purpleChar = $Purple
+onready var redChar = $Red
+onready var purpleWorld = $PurpleDimension
+onready var redWorld = $RedDimension
+
+onready var red = get_tree().get_nodes_in_group("red")
+onready var purple = get_tree().get_nodes_in_group("purple")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_level0()
+
+func _switch():
+	pass
+	
+func _level0():
+	#setting start positions for each character at the start of the level
+	purpleChar.position = $PurpleDimension/StartPosition.position
+	redChar.position = $RedDimension/StartPosition.position
+	
+	
+func _invisible(var array): #will take 
+	pass
+	
+	
+	
